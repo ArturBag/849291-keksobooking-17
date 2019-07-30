7f193e0094d2cc616d621fa795cb481bfe0d3c3f
Артур <artur8888b@gmail.com>
2019-07-30 22:50:00 +0300

    20. Личный проект: доверяй, но проверяй. Часть 2

--- js/form.js
+++ js/form.js
@@ -9,6 +9,8 @@
   var priceField = adForm.querySelector('#price');
   var timeIn = adForm.querySelector('#timein');
   var timeOut = adForm.querySelector('#timeout');
+  var rooms = adForm.querySelector('#room_number');
+  var roomsList = adForm.querySelectorAll('#room_number option');
   var capacity = adForm.querySelector('#capacity');
 
   var setMinPrice = function () {
@@ -43,6 +45,49 @@
       currentElement.setAttribute('disabled', true);
     }
   };
+
+  capacity.addEventListener('change', function (evt) {
+    var selectedOption = parseInt(evt.currentTarget.value);
+    if (selectedOption === 0) {
+      selectedOption = roomsList.length;
+    }
+    roomsList[selectedOption - 1].selected = true;
+  });
+
+  rooms.addEventListener('change', function () {
+    // var flag = false;
+    // console.log(rooms.length)
+    var capcityQty = capacity.value;
+    var roomsQty = rooms.value;
+    // console.log('capcityQty lenght', rooms.lenght, 'roomsQty lenght',capacity.lenght)
+
+    // if (capcityQty <= roomsQty && capcityQty !== 0) {
+    //   flag = true;
+    //   console.log('correct')
+
+    // } else if (capcityQty === 0 && roomsQty === 100) {
+    //   flag = true;
+    //   console.log('correct++')
+    // }
+
+    // else {
+    //   rooms.setCustomValidity('Количество комнат не соответствует количеству гостей');
+    //   flag = false;
+    //   console.log('NOTcorrect')
+    // }
+
+    // console.log(flag)
+    //console.log(capcityQty, roomsQty, capacity === 0, roomsQty < 100)
+    //console.log(rooms.selectedIndex, capacity.selectedIndex, rooms.selectedIndex != capacity.selectedIndex)
+    if (capacity.selectedIndex != rooms.length - 1 || roomsQty < capcityQty
+    ) {
+      rooms.setCustomValidity('Количество комнат не соответствует количеству гостей');
+      // console.log(roomsQty, capcityQty);
+    }
+
+
+
+  })
 
   window.form = {
     adForm: adForm,
